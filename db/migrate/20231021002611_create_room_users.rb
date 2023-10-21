7be9b6d19c14524ef25d20dc3b3d165c8f085321
ponponpoon <144258885+ponponpoon@users.noreply.github.com>
2023-10-21 16:44:22 +0900

    messagesテーブルの作成

--- db/migrate/20231021002611_create_room_users.rb
+++ db/migrate/20231021002611_create_room_users.rb
@@ -1,6 +1,7 @@
 class CreateRoomUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :room_users do |t|
+      t.string  :content
       t.references :room, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
       t.timestamps
